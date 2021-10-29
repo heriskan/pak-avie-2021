b8e13bf8200a8606f2030d3fa84e5477de16695e
Herry Iskandar <heriskan@gmail.com>
2021-10-29 12:35:15 +0700

    test foto

--- index.js
+++ index.js
@@ -5,33 +5,27 @@ var app = new Vue({
         message: 'selamat ulang tahun',
         avatars: [
             {
-                src: 'man1',
-                src2: 'woman9',
+                src: 'tuti',
                 aud: 'man1'
             },
             {
                 src: 'woman1',
-                src2: 'woman9',
                 aud: 'man1'
             },
             {
                 src: 'woman2',
-                src2: 'woman9',
                 aud: 'man1'
             },
             {
                 src: 'woman3',
-                src2: 'woman9',
                 aud: 'man1'
             },
             {
                 src: 'woman4',
-                src2: 'woman9',
                 aud: 'man1'
             },
             {
                 src: 'woman5',
-                src2: 'woman9',
                 aud: 'man1'
             }
         ]
@@ -39,7 +33,7 @@ var app = new Vue({
     },
     methods: {
         swap: (event, avatar, index) => {
-            event.target.src = "img/" + avatar.src2 + ".png";
+            event.target.src = "img/" + avatar.src + ".gif";
             var audio = app.$refs['aud-' + index][0];
             audio.play();
             setTimeout(
