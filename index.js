61ff8b141c0803e5d40f93f96c78c112e76e90d4
Herry Iskandar <heriskan@gmail.com>
2021-10-31 16:35:06 +0700

    ari adiba

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 var app = new Vue({
-    el: '#pbb-stickers',
-    data: {
+  el: '#pbb-stickers',
+  data: {
 
-        message: 'Silakan klik pada foto untuk aktifkan animasi dan suara.',
-        avatars: [
-            {
-                src: 'tuti',
-                nama: 'Tuti',
-                aud: 'man1'
-            },
-            {
-                src: 'sitta',
-                nama: 'Sitta',
-                aud: 'sitta'
-            },
-            {
-                src: 'hijri',
-                nama: 'Hijri',
-                aud: 'hijri'
-            },
-            {
-                src: 'anna',
-                nama: 'Anna',
-                aud: 'anna'
-            },
-            {
-                src: 'nova',
-                nama: 'Nova',
-                aud: 'man1'
-            },
-        ]
+    message: 'Silakan klik pada foto untuk aktifkan animasi dan suara.',
+    avatars: [
+      {
+        src: 'tuti',
+        nama: 'Tuti',
+        aud: 'man1'
+      },
+      {
+        src: 'sitta',
+        nama: 'Sitta',
+        aud: 'sitta'
+      },
+      {
+        src: 'hijri',
+        nama: 'Hijri',
+        aud: 'hijri'
+      },
+      {
+        src: 'anna',
+        nama: 'Anna',
+        aud: 'anna'
+      },
+      {
+        src: 'nova',
+        nama: 'Nova',
+        aud: 'man1'
+      },
+      {
+        src: 'ari',
+        nama: 'Ari',
+        aud: 'ari'
+      },
+      {
+        src: 'adiba',
+        nama: 'Adiba',
+        aud: 'adiba'
+      },
+    ]
 
-    },
-    methods: {
-        swap: (event, avatar, index) => {
-            event.target.src = "img/" + avatar.src + ".gif";
-            var audio = app.$refs['aud-' + index][0];
-            audio.play();
-            setTimeout(
-                function () { event.target.src = "img/" + avatar.src + ".png" }, audio.duration * 1000
-            )
-        }
+  },
+  methods: {
+    swap: (event, avatar, index) => {
+      event.target.src = "img/" + avatar.src + ".gif";
+      var audio = app.$refs['aud-' + index][0];
+      audio.play();
+      setTimeout(
+        function () { event.target.src = "img/" + avatar.src + ".png" }, audio.duration * 1000
+      )
     }
+  }
 })
