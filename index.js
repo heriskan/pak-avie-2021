17bda394e26427508629ec361a4fd590ceca01d1
Herry Iskandar <heriskan@gmail.com>
2021-10-31 17:04:07 +0700

    alfi alifa dan play stop audio

--- index.js
+++ index.js
@@ -7,49 +7,77 @@ var app = new Vue({
       {
         src: 'tuti',
         nama: 'Tuti',
-        aud: 'man1'
+        aud: 'man1',
+        play: false
       },
       {
         src: 'sitta',
         nama: 'Sitta',
-        aud: 'sitta'
+        aud: 'sitta',
+        play: false
       },
       {
         src: 'hijri',
         nama: 'Hijri',
-        aud: 'hijri'
+        aud: 'hijri',
+        play: false
       },
       {
         src: 'anna',
         nama: 'Anna',
-        aud: 'anna'
+        aud: 'anna',
+        play: false
       },
       {
         src: 'nova',
         nama: 'Nova',
-        aud: 'man1'
+        aud: 'man1',
+        play: false
       },
       {
         src: 'ari',
         nama: 'Ari',
-        aud: 'ari'
+        aud: 'ari',
+        play: false
       },
       {
         src: 'adiba',
         nama: 'Adiba',
-        aud: 'adiba'
+        aud: 'adiba',
+        play: false
+      },
+      {
+        src: 'alfi',
+        nama: 'Alfi',
+        aud: 'alfi',
+        play: false
+      },
+      {
+        src: 'alifa',
+        nama: 'Alifa',
+        aud: 'alifa',
+        play: false
       },
     ]
 
   },
   methods: {
     swap: (event, avatar, index) => {
-      event.target.src = "img/" + avatar.src + ".gif";
       var audio = app.$refs['aud-' + index][0];
-      audio.play();
-      setTimeout(
-        function () { event.target.src = "img/" + avatar.src + ".png" }, audio.duration * 1000
-      )
+      audio.volume = 0.4;
+      if (avatar.play == false) {
+        avatar.play = true
+        event.target.src = "img/" + avatar.src + ".gif";
+        audio.play();
+        setTimeout(
+          function () { event.target.src = "img/" + avatar.src + ".png" }, audio.duration * 1000
+        )
+      } else {
+        event.target.src = "img/" + avatar.src + ".png";
+        audio.pause();
+        audio.currentTime = 0;
+        avatar.play = false;
+      }
     }
   }
 })
